bfd13ce00126216c1022a355674139ae84982d7c
Jiuyang liu <liujiuyang1994@gmail.com>
2020-03-29 14:44:56 +0000

    add test

--- tests/src/TopWireSpec.scala
+++ tests/src/TopWireSpec.scala
@@ -4,35 +4,69 @@ import diplomatictester._
 import chisel3._
 import chisel3.util._
 import chisel3.stage.ChiselGeneratorAnnotation
+import chisel3.util.experimental.BoringUtils
 import firrtl.options.TargetDirAnnotation
 
-class InnerModule0 extends MultiIOModule {
-  val innerIO0 = IO(Decoupled(Bool()))
-  TopIO.addIO(innerIO0, "boring_")
+/** Module to be wired to top. */
+class MonitorModule extends MultiIOModule {
+  val o = IO(Decoupled(Bool()))
+  val i = IO(Flipped(Decoupled(Bool())))
+  TopIO.addIO(o, "out")
+  TopIO.addIO(i, "in")
+}
+
+class BufferModule extends MultiIOModule {
+  val o = IO(DecoupledIO(Bool()))
+  val i = IO(Flipped(DecoupledIO(Bool())))
+  val bits = RegNext(i.bits)
+  val v = RegNext(i.valid)
+  val r = RegNext(o.ready)
+  o.bits := bits
+  o.valid := v
+  i.ready := r
+}
+
+class MiddleModule extends MultiIOModule {
+  val o = IO(DecoupledIO(Bool()))
+  val i = IO(Flipped(DecoupledIO(Bool())))
+  val monitorInstance = Module(new MonitorModule)
+  val bufferInstance = Module(new BufferModule)
+  bufferInstance.i <> monitorInstance.o
+  o <> bufferInstance.o
+  monitorInstance.i <> i
 }
 
 class InnerModule1 extends MultiIOModule {
   val innerIO1 = IO(Decoupled(Bool()))
   innerIO1.valid := innerIO1.ready
-  val r = RegInit(0.U)
+  val r = RegInit(Bool())
   when(innerIO1.fire()) {
-    r := r + 1.U
+    r := !r
   }
   innerIO1.bits := r
 }
 
-class OuterModule extends MultiIOModule {
-  val innerInstance = Module(new InnerModule0)
-  val anotherInstance = Module(new InnerModule1)
-  val outerIO = IO(innerInstance.innerIO0.cloneType)
-  val outerIO1 = IO(anotherInstance.innerIO1.cloneType)
-  innerInstance.innerIO0 <> outerIO
-  anotherInstance.innerIO1 <> outerIO1
+class Top extends MultiIOModule {
+  val bufferInstance = Module(new BufferModule)
+  val bi = IO(Flipped(DecoupledIO(Bool())))
+  val bo = IO(DecoupledIO(Bool()))
+  bufferInstance.i <> bi
+  bo <> bufferInstance.o
+
+  val middleInstance = Module(new MiddleModule)
+  val mi = IO(Flipped(DecoupledIO(Bool())))
+  val mo = IO(DecoupledIO(Bool()))
+  middleInstance.i <> mi
+  middleInstance.o <> mo
+  val monitorO = IO(DecoupledIO(Bool()))
+  val monitorI = IO(Flipped(DecoupledIO(Bool())))
+  TopIO.getIO(monitorO, "out")
+  TopIO.getIO(monitorI, "in")
 }
 
 object Test extends App {
   (new chisel3.stage.ChiselStage).run(Seq(
-    ChiselGeneratorAnnotation(() => new OuterModule),
+    ChiselGeneratorAnnotation(() => new Top),
     TargetDirAnnotation("circuit")
   ))
 }
