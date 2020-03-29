a7a713e50e96d74b9858449a5e0b523a65385822
Jiuyang liu <liujiuyang1994@gmail.com>
2020-03-29 20:31:22 +0000

    wip

--- src/TopIOTransform.scala
+++ src/TopIOTransform.scala
@@ -31,7 +31,7 @@ class TopIOTransform extends Transform {
     /** TopModule Target. */
     val topModel: ModuleTarget = ModuleTarget(state.circuit.main, state.circuit.main)
     /** a tuple of [[InnerIOAnnotation]], [[TopIOAnnotation]] and io name. */
-    val ioPair: Seq[(ReferenceTarget, ReferenceTarget, String)] = {
+    val ioPairs: Map[String, (ReferenceTarget, ReferenceTarget)] = {
       /** get innerIOs. */
       val innerIOs = state.annotations.collect { case InnerIOAnnotation(target, name) => target -> name }
       innerIOs.groupBy(_._2).mapValues(_.size).foreach { case (name, num) =>
@@ -56,38 +56,75 @@ class TopIOTransform extends Transform {
       topDiffInner.foreach { name => throw new Exception(s"InnerIOAnnotation(someIO, $name) doesn't appear in TopIOAnnotation(someIO, $name)") }
       val topIOMap = topIOs.toMap
       innerIOs.map { case (t, n) =>
-        (t, topIOMap(n), instanceGraph.findInstancesInHierarchy(t.moduleTarget.module).head.foldLeft("_TOPIO")(_ + "_" + _.name))
+        instanceGraph.findInstancesInHierarchy(t.moduleTarget.module).head.foldLeft("_TOPIO")(_ + "_" + _.name) + t.name -> (t, topIOMap(n))
       }
-    }
-    val bottomModels: Seq[ModuleTarget] = ioPair.map(_._1.moduleTarget)
+      }.toMap
+    val bottomModels: Seq[ModuleTarget] = ioPairs.values.map(_._2.moduleTarget).toSeq
     /** collect port information from entire circuit. */
-    val ioInfo: Map[ReferenceTarget, (Type, Direction)] = state.circuit.modules.flatMap { m =>
-      m.ports.map {
-        p: Port => Target.asTarget(ModuleTarget(state.circuit.main, m.name))(WRef(p)) -> (p.tpe, p.direction)
+    val ioInfo: Map[String, (Type, Direction)] = state.circuit.modules.flatMap { m =>
+      m.ports.flatMap {
+        p: Port =>
+          val rt = Target.asTarget(ModuleTarget(state.circuit.main, m.name))(WRef(p))
+          ioPairs.find(_._2._1 == rt) match {
+            case None => Nil
+            case Some(pair) => Map(pair._1 -> (p.tpe, p.direction))
+          }
       }
     }.toMap
+    /** each module on the boring path will add new ports */
+    val moduleNewPortsMap: Map[ModuleTarget, Seq[String]] = {
+      ???
+    }
     val updatedModules: Seq[DefModule] = state.circuit.modules.map {
       module =>
 
         /** [[ModuleTarget]] based on [[DefModule]]. */
         val moduleTarget: ModuleTarget = ModuleTarget(state.circuit.main, module.name)
-        /** Namespace of thisModule. */
-        val namespace = Namespace(module)
+        val portTargets = module.ports.map(p => Target.asTarget(moduleTarget)(WRef(p)))
+        if (moduleNewPortsMap.contains(moduleTarget)) {
 
-        /** bottom module */
-        if (bottomModels.contains(moduleTarget)) {
-          module
+          /** bottom module */
+          if (bottomModels.contains(moduleTarget)) {
+            /** require all IO is annotated, since whole module will be replaced. */
+            val portTargets = module.ports.map(p => Target.asTarget(moduleTarget)(WRef(p)) -> p).toMap
+            require((portTargets.keys.toSeq diff ioPairs.map(_._2._1).toSeq).isEmpty, s"IO of bottom Moudle ${moduleTarget.name} are not fully annotated.")
+            val blocks = portTargets.map { case (rt, p) =>
+              val pair = ioPairs.find(_._2._1 == rt).get
+              val newPort = Port(NoInfo, pair._1, p.direction match {
+                case Input => Output
+                case Output => Input
+              }, p.tpe)
+              val newConnect = Connect(NoInfo, WRef(newPort), WRef(p))
+              (newPort, newConnect)
+            }.toSeq
+            Module(module.info, module.name, module.ports ++ blocks.map(_._1), Block(blocks.map(_._2)))
+          }
+
+          /** top module */
+          else if (moduleTarget == topModel) {
+            /** all annotated Top IOs. */
+            val portTargets = moduleNewPortsMap(moduleTarget).map(ioPairs).map(_._2)
+
+          }
+
+          /** middle module */
+          else {
+            /** new IOs. */
+            val newPorts = moduleNewPortsMap(moduleTarget).map(name => Port(NoInfo, name, ioInfo(name)._2, ioInfo(name)._1))
+            val portTargets = newPorts.map(p => Target.asTarget(moduleTarget)(WRef(p)) -> p).toMap
+            /** generate new block */
+            val newConnects = portTargets.map {
+              case (rt, p) =>
+
+                /** Filter sub-instance which contains this new IO. */
+                Connect(NoInfo, WSubField(WRef(???), p.name), WRef(p))
+            }
+          }
+
         }
 
-        /** top module */
-        else if (moduleTarget == topModel) {
-          module
-        }
-
-        /** middle module */
-        else {
-          module
-        }
+        /** other module */
+        else module
     }
     val newCircuit = state.circuit.copy(modules = updatedModules)
     val fixedCircuit = fixupCircuit(newCircuit)
